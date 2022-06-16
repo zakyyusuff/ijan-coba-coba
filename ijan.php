4d225295d0ed036ba8946ecc5cfefe2c58b3cd19
zaky yusuf <59588009+zakyyusuff@users.noreply.github.com>
2022-06-16 20:40:58 +0700

    Create ijan.php

---
+++ ijan.php
@@ -0,0 +1,183 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Apple official web</title>
+  </head>
+  <body> 
+  
+  	<!-- awal navbar -->
+  	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">The Product</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Iphone <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Ipad</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Mac</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Watch</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+  	<!-- akhir navbar -->
+
+
+<!-- awal jumbotron -->
+<div class="jumbotron jumbotron-fluid text-center">
+  <div class="container">
+  	<img src="img/Apple.png" width="300" class="rounded-circle">
+    <h1 class="display-4">IPHONE 12 (The Future Is Here)</h1>
+    <p class="lead">Your next computer is not a computer.</p>
+    <br>
+  </div>
+</div>
+<!-- akhir jumbrotron -->
+
+<div class="container">
+	<div class="row">
+		<div class="col text-center">
+			<h1>Why apple the best place to buy iphone</h1>
+		</div>
+	</div>
+	<!-- awal desc -->
+	<div class="row">
+		<div class="col text-center">
+			<!-- desc kiri -->
+			<p>You can choose a payment option that works for you, pay less with a trade‑in, connect your new iPhone to your carrier, and get set up quickly.
+You can also chat with a Specialist anytime ..</p>
+		</div>
+</div>
+	<!-- akhir desc -->
+
+	<!-- untuk enter -->
+<br>
+<br>
+<br>
+	<!-- untuk enter -->
+	
+	<!-- untuk card -->
+ 	<div class="container">
+ 		<div class="row">
+ 			<div class="col text-center">
+ 			<h1>Iphone 12 Series</h1>	
+ 			</div>
+ 		</div>
+ 		<!-- card -->
+ 		<div class="row">
+ 			<div class="col">
+ 				<!-- card 1 -->
+ 					<div class="card">
+  						<img class="card-img-top" src="img/iphone.jpg" alt="Card image cap">
+						  <div class="card-body">
+						    <h5 class="card-title">Iphone 12</h5>
+						    <p class="card-text">A14 Bionic is the fastest chip in a smartphone. And it pushes what’s possible. Like crunching trillions of operations on the Neural Engine. Or shooting in Dolby Vision — even pro movie cameras can’t do that. It’s superefficient for great battery life. And it’s ahead of its time, prepared to power pretty much anything that comes next.</p>
+						    <a href="https://shop.jtglobal.com/wp-content/uploads/2020/10/iphone-12-blue.jpg" class="btn btn-primary">View Image</a>
+						</div>
+					</div>
+ 				<!-- card 1 -->
+ 			</div>
+ 			<!-- card 2 -->
+ 			<div class="col">
+ 					<div class="card">
+  						<img class="card-img-top" src="img/iphonepro.jpg" alt="Card image cap">
+						  <div class="card-body">
+						    <h5 class="card-title">Iphone 12 Pro</h5>
+						    <p class="card-text">Vastly more contrast. Incredible color accuracy. A huge jump in pixel density.6
+Wow. Just wow. OLED delivers brighter brights, darker blacks, and higher resolution for everything you look at. This is the best iPhone display we’ve ever made, and it goes all the way to the edge.</p>
+						    <a href="https://www.phoneplacekenya.com/wp-content/uploads/2020/07/Apple-iPhone-12-Pro-c.jpg" class="btn btn-primary">View Image</a>
+						</div>
+					</div>
+ 			<!-- card 2 -->
+ 		</div>
+ 		<!-- card 3 -->
+				<div class="col">
+ 					<div class="card">
+  						<img class="card-img-top" src="img/iphonemini1.jpg" alt="Card image cap">
+						  <div class="card-body">
+						    <h5 class="card-title">Iphone 12 Mini</h5>
+						    <p class="card-text">How do you improve on the highest-quality video in a smartphone? Make it even better in low light — a full 27 percent better. That means no hour is off limits for more beautifully detailed video. Then there’s Night mode Time-lapse. Just prop up your iPhone and let the light trails begin.</p>
+						    <a href="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-12-family-select-2020?wid=940&amp;hei=1112&amp;fmt=jpeg&amp;qlt=80&amp;op_usm=0.5,0.5&amp;.v=1604343709000" class="btn btn-primary">View Image</a>
+						</div>
+					</div>
+
+ 		<!-- card 3 -->
+ 	<!-- space -->
+ 	<br>
+ 	<br>
+ 	<br>
+ 	<br>
+ 	<br>
+ 	<br>
+ 	<br>
+ 	<!-- akhir space -->
+
+ 		<!-- card -->
+ 	</div>
+
+ 	<!-- space -->
+ 	<br>
+ 	<br>
+ 	<br>
+ 	<br>
+ 	<br>
+ 	<br>
+
+
+ 	<!-- akhir space -->
+
+ <!-- space -->
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <!-- space -->
+
+
+	<!-- HAK CIPTA -->
+
+		<div class="alert alert-warning container text-center fixed-bottom alert-dismissible fade show" role="alert">
+  			<strong>This website is made by</strong> IZZAN FAKIH ABIA HANIF.
+  				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    		<span aria-hidden="true">&times;</span>
+  		</button>
+	</div>
+	<!-- HAK CIPTA -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
